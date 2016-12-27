b4cee2f01ed43fb50c3328dd1cddcf2964a75ca9
Lennard Berger <berger@mecodia.de>
2016-12-27 18:57:35 +0100

    Add's current implementation stash.

---
+++ src/configurator/index.js
@@ -0,0 +1,38 @@
+/**
+ * @module configurator
+ * Traverses a folder and searches for valid TOML documents
+ * Builds an insertable tree object
+ */
+
+const tree = require('tree');
+const buckets = require('buckets-js');
+const glob = require('glob');
+const toml-node = require('toml-node');
+
+/**
+ * configurator function
+ * @param  {string} path - the oath to traverse
+ * @return {Promise}      returns a Promise containing a {@link tree} of parsed TOML objects
+ */
+module.exports = function(path) {
+  return new Promise((resolve, reject) => {
+    glob('**/*.toml', {cwd: path}, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+
+      let config_tree = buckets.BTree();
+      files.forEach((file) => {
+        try {
+          let data = toml.parse(file);
+          for (entry in Object.entries(data)) {
+            override(entry, config_tree);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      })
+      resolve(config_tree);
+    })
+  })
+}
---
+++ src/tree/index.js
@@ -0,0 +1,17 @@
+/**
+ * @module tree
+ * Provides generic methods to work with bucket-js trees
+ * @see buckets-js
+ */
+
+
+module.exports = {
+  /**
+   * Overrides a member in the tree and it's chidlren recursively
+   * @param  {Object} obj
+   * @param  {BSTree} tree
+   */
+  override (obj, tree) {
+    
+  }
+}
