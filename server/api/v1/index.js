78977522ffad4635926a775084fbbba01beb2d47
Fohlen <lennard@berger.work>
2017-01-04 23:03:22 +0100

    Fix the RESTfull API. You can now create instances! Heureka!

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -19,23 +19,25 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 var root = new tree.Root();
+// NOTE: This might be changed in the future where trees can be im/exported
+if (!root.contains('/instances')) {
+  root.addChild('instances', 'node');
+}
 
 // Lists all available instances
 router.get('/instances', (req, res) => {
-  if (!root.contains('/instances')) {
-    root.addChild('instances', 'node');
-  }
   let instances = root.findNode('/instances')
-  res.json(instances.toString());
+  console.log(root);
+  res.type('json').send(instances.toString());
 })
 
 // Lists information about a given instance or raises a NonFoundError
 router.get('/instances/:id/', (req, res) => {
-  if (root.contains('/instances/' + req.params.id)) {
+  if (!root.contains('/instances/' + req.params.id)) {
     res.status(500).send('Instance with id ' + req.params.id + ' was not found');
   } else {
-    let instance = root.findNode('/instances/' + req.params.id).get();
-    res.json(instance.toString());
+    let node = root.findNode('/instances/' + req.params.id);
+    res.json(node.get());
   }
 })
 
@@ -46,8 +48,9 @@ router.post('/instances/:id/', (req, res) => {
       res.status(500).send('Instance can not be created without command line arguments.');
     } else {
       manager.create(req.body.args, req.params.id, req.body.port).then((instance) => {
-        let _i = root.findNode('/instances').addChild(instance.id, 'node', instance);
-        res.json(_i.toString);
+        let node = root.findNode('/instances').addChild(String(instance.id), 'flex', instance);
+        console.log(node);
+        res.json(node.get());
       }).catch((err) => {
         res.status(500).send(err);
       })
@@ -60,10 +63,10 @@ router.post('/instances/:id/', (req, res) => {
 // Starts an instance with :id. Returns the started instance or raises an error.
 router.get('/instances/:id/start', (req, res)  => {
   if (root.contains('/instance/' + req.params.id)) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
-    manager.start(instance).then((i) => {
-      instance.set(i);
-      res.json(i.toString());
+    let node = root.findNode('/instance').getChild(req.params.id);
+    manager.start(node.get()).then((instance) => {
+      node.set(i);
+      res.json(instance);
     }).catch((err) => {
       res.status(500).send(err);
     })
@@ -75,10 +78,10 @@ router.get('/instances/:id/start', (req, res)  => {
 // Stops an instance with :id. Returns the stoped instance or raises an error.
 router.get('/instances/:id/stop', (req, res)  => {
   if (root.contains('/instance/' + req.params.id)) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
-    manager.stop(instance).then((i) => {
-      instance.set(i);
-      res.json(i.toString());
+    let node = root.findNode('/instance').getChild(req.params.id);
+    manager.stop(node.get()).then((instance) => {
+      node.set(instance);
+      res.json(instance);
     }).catch((err) => {
       res.status(500).send(err);
     })
@@ -90,7 +93,8 @@ router.get('/instances/:id/stop', (req, res)  => {
 // Connects an instance with :id to Inexor Core. Returns the connected instance or raises an error.
 router.get('/instance/:id/connect', (req, res) => {
   if (root.contains('/instance/' + req.params.id)) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
+    let node = root.findNode('/instance').getChild(req.params.id);
+    let instance = node.get();
     let connector = new Connector(instance.port, instance.tree);
 
     try {
@@ -109,7 +113,8 @@ router.get('/instance/:id/connect', (req, res) => {
 // Synchronizes an instance with Inexor Core. Returns the synchronized instance or raises an error.
 router.get('/instance/:id/synchronize', (req, res) => {
   if (root.contains('/instance/' + req.params.id)) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
+    let node = root.findNode('/instance').getChild(req.params.id);
+    let instance = node.get();
 
     if (instance._connector) {
       instance._connector._initialize();
@@ -130,9 +135,14 @@ router.get('/instance/:id/configure', (req, res) => {
 // Set and get keys from instance/id's tree.
 router.get('/tree/:id/:path', (req, res) => {
   if (root.contains('/instance/') + req.params.id) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
+    let node = root.findNode('/instance').getChild(req.params.id);
+    let instance = node.get();
     if (instance.tree.contains(req.params.path)) {
-      res.json(String(instance.tree.findNode(req.params.path)));
+      if (instance.tree.findNode(req.params.path) == 'node') {
+        res.type('json').send(instance.tree.findNode(req.params.path).toString());
+      } else {
+        res.json(instance.tree.findNode(req.params.path).get())
+      }
     } else {
       res.status(500).send('Key with path ' + req.params.path + ' was not found');
     }
@@ -143,10 +153,15 @@ router.get('/tree/:id/:path', (req, res) => {
 
 router.post('/tree/:id/:path', (req, res) => {
   if (root.contains('/instance/') + req.params.id) {
-    let instance = root.findNode('/instance').getChild(req.params.id);
+    let node = root.findNode('/instance').getChild(req.params.id);
+    let instance = node.get();
     if (instance.tree.contains(req.params.path)) {
-      instance.tree.findNode(req.params.path).set(req.body.value);
-      res.status(200);
+      if (instance.tree.findNode(req.params.path)._datatype == 'node') {
+        res.status(500).send('Synchronizing nodes is not possible.')
+      } else {
+        instance.tree.findNode(req.params.path).set(req.body.value);
+        res.status(200);
+      }
     } else {
       res.status(500).send('Key with path ' + req.params.path + ' was not found');
     }
