13a4a8f0d8330c267625c62372d0b92377b09bd4
Andreas Schaeffer <hanack@nooblounge.net>
2017-01-07 15:13:16 +0100

    Fixed API paths and use hasChild and getChild

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -7,6 +7,8 @@
 const process = require('process');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bunyan = require('bunyan');
+const bunyanDebugStream = require('bunyan-debug-stream');
 
 // Pull the inexor dependencies
 const tree = require('@inexor-game/tree');
@@ -14,42 +16,53 @@ const manager = require('@inexor-game/manager');
 const connector = require('@inexor-game/connector');
 // const configurator = require('@inexor-game/configurator');
 
+var log = bunyan.createLogger({
+	name: '@inexor-game/api_v1',
+	streams: [{
+ 		level: 'debug',
+ 		type: 'raw',
+ 		stream: bunyanDebugStream({
+ 			forceColor: true
+ 		})
+	}],
+	serializers: bunyanDebugStream.serializers
+});
+
 var router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 var root = new tree.Root();
 // NOTE: This might be changed in the future where trees can be im/exported
-if (!root.contains('/instances')) {
-  root.addChild('instances', 'node');
+var instances = null;
+if (!root.hasChild('instances')) {
+  instances = root.addChild('instances', 'node');
 }
 
 // Lists all available instances
 router.get('/instances', (req, res) => {
-  let instances = root.findNode('/instances')
-  console.log(root);
-  res.type('json').send(instances.toString());
+  res.type('json').send(instances.get());
 })
 
 // Lists information about a given instance or raises a NonFoundError
-router.get('/instances/:id/', (req, res) => {
-  if (!root.contains('/instances/' + req.params.id)) {
-    res.status(500).send('Instance with id ' + req.params.id + ' was not found');
-  } else {
-    let node = root.findNode('/instances/' + req.params.id);
-    res.json(node.get());
-  }
+router.get('/instances/:id', (req, res) => {
+	if (instances.hasChild(req.params.id)) {
+		let node = instances.getChild(req.params.id);
+		res.json(node.get());
+	} else {
+		res.status(500).send('Instance with id ' + req.params.id + ' was not found');
+	}
 })
 
 // Creates an instance and inserts it into the tree. Returns the instance object, otherwise raises an error.
-router.post('/instances/:id/', (req, res) => {
-  if (!root.contains('/instances/' + req.params.id)) {
+router.post('/instances/:id', (req, res) => {
+  if (!instances.hasChild(req.params.id)) {
     if (req.body.args == null) {
       res.status(500).send('Instance can not be created without command line arguments.');
     } else {
       manager.create(req.body.args, req.params.id, req.body.port).then((instance) => {
-        let node = root.findNode('/instances').addChild(String(instance.id), 'flex', instance);
-        console.log(node);
+      	let node = instances.addChild(String(instance.id), 'flex', instance);
+      	log.info("Successfully created instance: " + node.getPath());
         res.json(node.get());
       }).catch((err) => {
         res.status(500).send(err);
@@ -62,23 +75,24 @@ router.post('/instances/:id/', (req, res) => {
 
 // Starts an instance with :id. Returns the started instance or raises an error.
 router.get('/instances/:id/start', (req, res)  => {
-  if (root.contains('/instance/' + req.params.id)) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     manager.start(node.get()).then((instance) => {
-      node.set(i);
+      node.set(instance);
       res.json(instance);
+      // TODO: res.json(node.get());
     }).catch((err) => {
       res.status(500).send(err);
     })
   } else {
-    res.status(500).send('Instance ' + req.params.id + ' was not found.');
+    res.status(500).send('Cannot start instance ' + req.params.id + '! Instance does not exist. You have to create an instance first.');
   }
 })
 
 // Stops an instance with :id. Returns the stoped instance or raises an error.
 router.get('/instances/:id/stop', (req, res)  => {
-  if (root.contains('/instance/' + req.params.id)) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     manager.stop(node.get()).then((instance) => {
       node.set(instance);
       res.json(instance);
@@ -86,14 +100,14 @@ router.get('/instances/:id/stop', (req, res)  => {
       res.status(500).send(err);
     })
   } else {
-    res.status(500).send('Instance ' + req.params.id + ' was not found.');
+    res.status(500).send('Cannot stop instance ' + req.params.id + '! Instance does not exist.');
   }
 })
 
 // Connects an instance with :id to Inexor Core. Returns the connected instance or raises an error.
-router.get('/instance/:id/connect', (req, res) => {
-  if (root.contains('/instance/' + req.params.id)) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+router.get('/instances/:id/connect', (req, res) => {
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     let instance = node.get();
     let connector = new Connector(instance.port, instance.tree);
 
@@ -106,14 +120,14 @@ router.get('/instance/:id/connect', (req, res) => {
     }
 
   } else {
-    res.status(500).send('Instance ' + req.params.id + ' was not found.');
+    res.status(500).send('Cannot connect with instance ' + req.params.id + '! Instance does not exist.');
   }
 })
 
 // Synchronizes an instance with Inexor Core. Returns the synchronized instance or raises an error.
-router.get('/instance/:id/synchronize', (req, res) => {
-  if (root.contains('/instance/' + req.params.id)) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+router.get('/instances/:id/synchronize', (req, res) => {
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     let instance = node.get();
 
     if (instance._connector) {
@@ -123,7 +137,7 @@ router.get('/instance/:id/synchronize', (req, res) => {
       res.status(500).send('There is no connector for instance ' + req.params.id);
     }
   } else {
-    res.status(500).send('Instance ' + req.params.id + ' was not found.');
+    res.status(500).send('Cannot synchronize with instance ' + req.params.id + '! Instance does not exist.');
   }
 })
 
@@ -134,8 +148,8 @@ router.get('/instance/:id/configure', (req, res) => {
 
 // Set and get keys from instance/id's tree.
 router.get('/tree/:id/:path', (req, res) => {
-  if (root.contains('/instance/') + req.params.id) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     let instance = node.get();
     if (instance.tree.contains(req.params.path)) {
       if (instance.tree.findNode(req.params.path) == 'node') {
@@ -152,8 +166,8 @@ router.get('/tree/:id/:path', (req, res) => {
 })
 
 router.post('/tree/:id/:path', (req, res) => {
-  if (root.contains('/instance/') + req.params.id) {
-    let node = root.findNode('/instance').getChild(req.params.id);
+  if (instances.hasChild(req.params.id)) {
+    let node = instances.getChild(req.params.id);
     let instance = node.get();
     if (instance.tree.contains(req.params.path)) {
       if (instance.tree.findNode(req.params.path)._datatype == 'node') {
