b2213b4d0610e23bb10cf355ebd12e602d326e21
Andreas Schaeffer <hanack@nooblounge.net>
2017-01-11 16:54:48 +0100

    Added window and shutdown functionality in the treeclient

--- src/treeclient/TreeClient.js
+++ src/treeclient/TreeClient.js
@@ -1,4 +1,5 @@
 const Client = require('node-rest-client').Client;
+const debuglog = require('util').debuglog('cmd-client-list');
 
 /**
  * The client for the local or remote Inexor Tree instances via a REST API.
@@ -13,23 +14,34 @@ class TreeClient {
     this.base_url = 'http://' + hostname + ':' + port + '/api/v' + api_version;
     this.flex = {
       instances: {
-        getAll: this.createEndpoint('/instances', 'getAllInstances'),
-        get: this.createEndpoint('/instances/${id}', 'getInstance'),
-        create: this.createEndpoint('/instances/${id}', 'createInstance', 'POST'),
-        remove: this.createEndpoint('/instances/${id}', 'removeInstance', 'DELETE'),
-        start: this.createEndpoint('/instances/${id}/start', 'startInstance'),
-        stop: this.createEndpoint('/instances/${id}/stop', 'stopInstance'),
-        connect: this.createEndpoint('/instances/${id}/connect', 'connectInstance'),
-        synchronize: this.createEndpoint('/instances/${id}/synchronize', 'synchronizeInstance')
-      }
+        getAll: this.createEndpoint('/instances', this.getAllInstances.name),
+        get: this.createEndpoint('/instances/${id}', this.getInstance.name),
+        create: this.createEndpoint('/instances/${id}', this.createInstance.name, 'POST'),
+        remove: this.createEndpoint('/instances/${id}', this.removeInstance.name, 'DELETE'),
+        start: this.createEndpoint('/instances/${id}/start', this.startInstance.name),
+        startAll: this.createEndpoint('/instances/start', this.startAllInstances.name),
+        stop: this.createEndpoint('/instances/${id}/stop', this.stopInstance.name),
+        stopAll: this.createEndpoint('/instances/stop', this.stopAllInstances.name),
+        connect: this.createEndpoint('/instances/${id}/connect', this.connectInstance.name),
+        synchronize: this.createEndpoint('/instances/${id}/synchronize', this.synchronizeInstance.name),
+        client: {
+          window: {
+            fullscreen: this.createEndpoint('/instances/${id}/window/fullscreen/${mode}', this.clientWindowFullscreen.name),
+            maximize: this.createEndpoint('/instances/${id}/window/maximize', this.clientWindowMaximize.name),
+            minimize: this.createEndpoint('/instances/${id}/window/minimize', this.clientWindowMinimize.name),
+            restore: this.createEndpoint('/instances/${id}/window/restore', this.clientWindowRestore.name)
+          }
+        }
+      },
+      shutdown: this.createEndpoint('/shutdown', 'shutdownFlex')
     }
   }
 
   createEndpoint(url, methodName, httpMethod = 'GET') {
     this.client.registerMethod(methodName, this.getEndpointUrl(url), httpMethod);
     var self = this;
-    var fwrapper = function() {
-      self[methodName](arguments);
+    var fwrapper = function(...args) {
+      self[methodName](...args);
     }
     return fwrapper;
   }
@@ -74,8 +86,16 @@ class TreeClient {
     this.callEndpoint(this.startInstance.name, callback, { id: id });
   }
 
+  startAllInstances(id, callback) {
+    this.callEndpoint(this.startAllInstances.name, callback);
+  }
+
   stopInstance(id, callback) {
     this.callEndpoint(this.stopInstance.name, callback, { id: id });
+  }
+
+  stopAllInstances(callback) {
+    this.callEndpoint(this.stopAllInstances.name, callback);
   }
 
   connectInstance(id, callback) {
@@ -84,6 +104,26 @@ class TreeClient {
 
   synchronizeInstance(id, callback) {
     this.callEndpoint(this.synchronizeInstance.name, callback, { id: id });
+  }
+
+  shutdownFlex(callback) {
+    this.callEndpoint(this.shutdownFlex.name, callback);
+  }
+
+  clientWindowFullscreen(id, mode, callback) {
+    this.callEndpoint(this.clientWindowFullscreen.name, callback, { id: id, mode: mode });
+  }
+
+  clientWindowMaximize(id, callback) {
+    this.callEndpoint(this.clientWindowMaximize.name, callback, { id: id });
+  }
+
+  clientWindowMinimize(id, callback) {
+    this.callEndpoint(this.clientWindowMinimize.name, callback, { id: id });
+  }
+
+  clientWindowRestore(id, callback) {
+    this.callEndpoint(this.clientWindowRestore.name, callback, { id: id });
   }
 
 }
