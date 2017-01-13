5aabfe3c5e5c4bd450671159154cb01e0b23a947
Andreas Schaeffer <hanack@nooblounge.net>
2017-01-13 19:31:01 +0100

    Prevent starting multiple instances of Inexor Flex.
    The PID file is written to /tmp/ currently. For most linuxes /var/run
    would be the correct place, but windows doesn't have a PID folder.
    
    Also added two shutdown hooks for SIGINT (Ctrl+C) and SIGTERM (kill
    <pid>) which removes the pid file.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 // Configures yargs to use the command directory
 const process = require('process');
+const os = require('os');
+const npid = require('npid');
 const path = require('path');
 const argv = require('yargs')
   .commandDir('commands')
   .demandCommand(1)
   .help()
-  .argv
+  .argv;
 
+//Returns a logger instance
+var log = require('@inexor-game/logger')(argv.console, argv.file, argv.level);
+
+// Prevent starting multiple instances of Inexor Flex
+var pid = null;
+try {
+  var pid_path = os.tmpdir() + '/inexor.pid';
+  pid = npid.create(pid_path);
+  pid.removeOnExit();
+} catch (err) {
+  log.error(err.message);
+  process.exit(1);
+}
 
 // Pull the dependencies
 const express = require('express');
-
-// Returns a logger instance
-var log = require('@inexor-game/logger')(argv.console, argv.file, argv.level)
 
 // Configures the server to be use-able as a RESTfull API
 var app = express();
@@ -24,13 +36,13 @@ app.use(express.static(path.resolve(argv.webdir)));
 app.use((req, res, next) => {
   log.info('Called route ' + req.path);
   next();
-})
+});
 
 // Handle errors
 app.use((err, req, res, next) => {
   log.error(err);
   next(err);
-})
+});
 
 // Require the router from the rest module
 var api = require('@inexor-game/api').v1;
@@ -44,6 +56,22 @@ require('@inexor-game/plugins').then((router) => {
 // Fire in the hole!
 app.use('/api/v1/', api); // This is assembled before runtime
 
-app.listen(argv.port, () => {
-  log.info('Inexor Flex is listening on ' + argv.port)
-})
+var server = app.listen(argv.port, (err) => {
+  if (err) {
+    log.error(err);
+  } else {
+    log.info('Inexor Flex is listening on ' + argv.port);
+  }
+});
+
+process.on('SIGINT', () => {
+  log.info('Got SIGINT. Graceful shutdown start', new Date().toISOString())
+  pid.remove();
+  process.exit();
+});
+
+process.on('SIGTERM', () => {
+  log.info('Got SIGTERM. Graceful shutdown start', new Date().toISOString())
+  pid.remove();
+  process.exit();
+});
