d3939243b2ab06c4f670e946caa60bfcee94b16b
Andreas Schaeffer <hanack@nooblounge.net>
2017-03-19 22:28:45 +0100

    Command create instance allows to start the instance

--- server/commands/cli/client_cmds/create.js
+++ server/commands/cli/client_cmds/create.js
@@ -2,7 +2,7 @@ const TreeClient = require('@inexor-game/treeclient').TreeClient;
 const log = require('@inexor-game/logger')();
 
 // Configuration for creating a client instance of Inexor Core
-exports.command = 'create <instance> [port] [name] [description]'
+exports.command = 'create <instance> [port] [name] [description] [start]'
 exports.describe = 'Creates a client'
 
 exports.builder = {
@@ -24,6 +24,11 @@ exports.builder = {
     default: '',
     type: 'string',
     describe: 'A description of the instance.'
+  },
+  start: {
+    type: 'boolean',
+    descibe: 'Also starts the created instance',
+    default: false
   }
 }
 
@@ -33,6 +38,26 @@ exports.handler = function(argv) {
   client.flex.instances.create(argv.instance, 'client', argv.port, argv.name, argv.description, function(data, response) {
     if (response.statusCode == 201) {
       log.info('Client with instance id ' + argv.instance + ' created');
+      if (argv.start) {
+        client.flex.instances.start(argv.instance, function(data, response) {
+          log.info('  Result: ' + response.statusCode + '(' + response.statusMessage + ')');
+          setTimeout(function() {
+            log.info('Connecting to the client with id ' + argv.instance);
+            client.flex.instances.connect(argv.instance, function(data, response) {
+              log.info('  Result: ' + response.statusCode + '(' + response.statusMessage + ')');
+              if (response.statusCode == 200) {
+                log.info('Connection to client with instance id ' + argv.instance + ' established');
+              } else if (response.statusCode == 404) {
+                log.info('Client with instance id ' + argv.instance + ' does not exist');
+              } else {
+                log.info('Response: ' + response.statusCode + ' ' + response.statusMessage);
+              }
+            });
+          }, 200);
+        });
+      } else {
+        log.warn('The client was started but no connection is established.');
+      }
     } else if (response.statusCode == 409) {
       log.info('Client with instance id ' + argv.instance + ' already exists');
     } else {
