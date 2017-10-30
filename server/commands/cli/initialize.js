0bcb6edd8085e5031d7acb00ddeb43591c6cf7a9
Andreas Schaeffer <hanack@nooblounge.net>
2017-10-30 14:33:31 +0100

    Disable lint rule for whole block

--- server/commands/cli/initialize.js
+++ server/commands/cli/initialize.js
@@ -4,21 +4,22 @@ const TreeClient = require('@inexorgame/treeclient').TreeClient;
 const path = require('@inexorgame/path');
 const log = require('@inexorgame/logger')();
 
-exports.command = 'initialize'
-exports.describe = 'Exports the instance according to the config'
+exports.command = 'initialize';
+exports.describe = 'Exports the instance according to the config';
 
 exports.handler = function(argv) {
   log.info('Reading file instances.toml');
   let client = new TreeClient('localhost', path.DEFAULT_PORT);
   client.flex.plugins.tomlreader(path.config_path + '/instances.toml', (instances) => {
     // For an empty tree this does nothing
-    for(instance in instances) { // eslint-disable-line no-undef
-      let id = instance._name.split('.')[1]; // eslint-disable-line no-undef
+    /* eslint-disable no-undef */
+    for(instance in instances) {
+      let id = instance._name.split('.')[1];
       // instances.number => number
-      let type = instance.getChild('type').get(); // eslint-disable-line no-undef
-      let name = instance.getChild('name').get(); // eslint-disable-line no-undef
-      let description = instance.getChild('description').get(); // eslint-disable-line no-undef
-      let autostart = instance.getChild('autostart').get(); // eslint-disable-line no-undef
+      let type = instance.getChild('type').get();
+      let name = instance.getChild('name').get();
+      let description = instance.getChild('description').get();
+      let autostart = instance.getChild('autostart').get();
       client.flex.instances.create(id, type, null, name, description, (data, response) => {
         if (response.statusCode == 201) {
           log.info(type + ' with instance id ' + argv.instance + ' created');
@@ -28,9 +29,10 @@ exports.handler = function(argv) {
             if (response.statusCode == 200) {
               log.info(type + ' with instance id ' + argv.instance + ' started');
             }
-          })
+          });
         }
-      })
+      });
     }
-  })
+    /* eslint-enable no-undef */
+  });
 }
