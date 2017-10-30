56e8ae64beedc9f0039ac57a60dfffe806d56cd1
a_teammate <madoe3@web.de>
2017-10-30 02:17:43 +0100

    [releases] make channel * match any.

--- server/commands/cli/releases_cmd/uninstall.js
+++ server/commands/cli/releases_cmd/uninstall.js
@@ -1,8 +1,20 @@
 const TreeClient = require('@inexorgame/treeclient').TreeClient;
 const log = require('@inexorgame/logger')();
 
-exports.command = 'uninstall <version> [<channel>]';
+exports.command = 'uninstall <version> [channel]';
 exports.describe = 'Uninstall a specific release';
+
+exports.builder = {
+    version: {
+        type: 'string',
+        describe: 'The semantic version range. If given, the newest version gets deinstalled.'
+    },
+    channel: {
+        default: '*',
+        type: 'string',
+        describe: 'If given and not "*" only versions in this particular channel get deinstalled.'
+    }
+};
 
 exports.handler = function(argv) {
     log.info(`Uninstalling release with version ${argv.version} @ ${argv.channel} via the command-line`);
