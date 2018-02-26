486b6bdbeb7cc3617690691bfca38a205f345b27
Croydon <cr0ydon@outlook.com>
2018-02-26 20:42:18 +0100

    fix(ci): Fix release type detection + shared vars

--- tool/travis_publish_npm.sh
+++ tool/travis_publish_npm.sh
@@ -8,10 +8,10 @@ export branch=${TRAVIS_BRANCH}
 
 if [ "$branch" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     # direct push to master
-    echo "This is a direct push to master. \n\n"
+    echo -e "This is a direct push to master. \n\n"
 else
     echo >&2 -e "\n===============\n" \
-    "Skipping publishing, because this is \n" \
+    "Skipping publishing, because this is\n" \
     "not a direct commit to master.\n" \
     "Branch: ${branch} - TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}\n" \
     "===============\n"
@@ -19,33 +19,38 @@ else
 fi
 
 
-if ! [ "$INEXOR_PATCH_VERSION" = "0" ]; then
-    if [ "$main_repo" = "inexorgame/inexor-flex" ]; then
-        echo >&2 -e "\n===============\n" \
-        "New Major or Minor version. \n" \
-        "Do nothing. The npm package is already published. \n" \
-        "===============\n"
-        exit 0
-    fi
+if ! [ "$main_repo" = "inexorgame/inexor-flex" ]; then
+    echo >&2 -e "\n===============\n" \
+    "Skipping publishing, because this is\n" \
+    "a fork.\n" \
+    "Repo: ${main_repo} - expected: inexorgame/inexor-flex\n" \
+    "===============\n"
+    exit 0
 fi
 
-if [ "$main_repo" = "inexorgame/inexor-flex" ]; then
-    if ! [ "$PUBLISH_NEW_MINOR" = "true" ]; then
+
+if ! [ "$PUBLISH_NEW_MINOR" = "true" ]; then
+    if [ "$INEXOR_PATCH_VERSION" = "0" ]; then
+        echo >&2 -e "\n===============\n" \
+        "New Major or Minor version detected. \n" \
+        "===============\n"
+    else
         echo >&2 -e "\n===============\n" \
         "This is executed by the push of a tag. \n" \
-        "Do nothing. The npm package is already published. \n" \
-        "===============\n"
-        exit 0
-    fi
-
-    if [[ "`git log -1 --pretty=%B`" == *"Rolling release: Increase version to"* ]]; then
-        echo >&2 -e "\n===============\n" \
-        "This is executed by a commit of a rolling release. \n" \
-        "Do nothing. The npm package is already published. \n" \
+        "Do nothing. The yarn package is already published. \n" \
         "===============\n"
         exit 0
     fi
 fi
+
+if [[ "`git log -1 --pretty=%B`" == *"Rolling release: Increase version to"* ]]; then
+    echo >&2 -e "\n===============\n" \
+    "This is executed by a commit of a rolling release. \n" \
+    "Do nothing. The yarn package is already published. \n" \
+    "===============\n"
+    exit 0
+fi
+
 
 
 git config --global user.email "ci@inexor.org"
