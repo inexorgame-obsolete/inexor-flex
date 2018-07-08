24f7a16669d807c45221e367241d4a7da588e939
Croydon <cr0ydon@outlook.com>
2018-07-08 03:23:19 +0200

    Dep check: Update and fix ignore modules

--- depcheck.js
+++ depcheck.js
@@ -7,21 +7,21 @@ const pjson = require('./package.json');
 /* TODO: Ignore all fileDep modules, then enable in CI */
 let ignoreModules = "";
 Object.entries(pjson.fileDependencies).forEach(([key, value]) => {
-    ignoreModules += `${key} `;
+    ignoreModules += `--ignore-module="${key}" `;
 });
 
-ignoreModules += "shelljs jsdoc mocha chai chai-iterator "
+ignoreModules += ` --ignore-module="shelljs" --ignore-module="jsdoc" --ignore-module="mocha" --ignore-module="chai" --ignore-module="chai-iterator" `
 
 shell.echo(`./package.json:`);
 shell.exec(`dependency-check ./package.json"`);
-shell.exec(`dependency-check ./package.json --unused --no-dev" --ignore-module="${ignoreModules}"`);
+shell.exec(`dependency-check ./package.json --unused --no-dev ${ignoreModules}`);
 shell.echo(` `);
 shell.echo(` `);
 
 Object.entries(pjson.fileDependencies).forEach(([key, value]) => {
     shell.echo(`${value}package.json:`);
     shell.exec(`dependency-check ${value}package.json --entry index.js`);
-    shell.exec(`dependency-check ${value}package.json --unused --entry index.js --ignore-module="${ignoreModules}"`);
+    shell.exec(`dependency-check ${value}package.json --unused --entry index.js ${ignoreModules}`);
     shell.echo(` `);
     shell.echo(` `);
 });
